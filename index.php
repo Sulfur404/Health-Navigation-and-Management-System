0321b4c6f3f70bc397409bbad77bc59de5e2a8d5
Sulfur404 <mrnafis10@gmail.com>
2025-09-26 01:11:54 +0600

    error fixed

--- index.php
+++ index.php
@@ -53,6 +53,83 @@
         </div>
     </div>
 
+        <div class="container-fluid bg-danger txt-banner">
+        <div class="row bottom-banner">
+          <div class="col-sm">
+            <h5> <i class="fas fa-book-open mr-3"></i> 100+ Online Courses</h5>
+          </div>
+          <div class="col-sm">
+            <h5><i class="fas fa-users mr-3"></i> Expert Instructors</h5>
+          </div>
+          <div class="col-sm">
+            <h5><i class="fas fa-keyboard mr-3"></i> Lifetime Access</h5>
+          </div>
+          <div class="col-sm">
+            <h5><i class="fas fa-rupee-sign mr-3"></i> Money Back Guarantee*</h5>
+          </div>
+        </div>
+    </div>
+
+    <div class="container mt-5"> <!-- Start Course -->
+      <h1 class="text-center">Popular Course</h1>
+      
+      <!-- <div class="card-deck mt-4"> //1st course cart
+        <?php
+        $sql = "SELECT * FROM course LIMIT 3";
+        $result = $conn->query($sql);
+        if($result->num_rows > 0){ 
+          while($row = $result->fetch_assoc()){
+            $course_id = $row['course_id'];
+            echo '
+            <a href="coursedetails.php?course_id='.$course_id.'" class="btn" style="text-align: left; padding:0px; margin:0px;">
+              <div class="card">
+                <img src="'.str_replace('..', '.', $row['course_img']).'" class="card-img-top" alt="Guitar" />
+                <div class="card-body">
+                  <h5 class="card-title">'.$row['course_name'].'</h5>
+                  <p class="card-text">'.$row['course_desc'].'</p>
+                </div>
+                <div class="card-footer">
+                  <p class="card-text d-inline">Price: <small><del>&#8377 '.$row['course_original_price'].'</del></small> <span class="font-weight-bolder">&#8377 '.$row['course_price'].'<span></p> <a class="btn btn-primary text-white font-weight-bolder float-right" href="coursedetails.php?course_id='.$course_id.'">Enroll</a>
+                </div>
+              </div>
+            </a>  ';
+          }
+        }
+        ?>   
+      </div>  
+      <div class="card-deck mt-4"> //2nd cart course
+        <?php
+          $sql = "SELECT * FROM course LIMIT 3,3";
+          $result = $conn->query($sql);
+          if($result->num_rows > 0){ 
+            while($row = $result->fetch_assoc()){
+              $course_id = $row['course_id'];
+              echo '
+                <a href="coursedetails.php?course_id='.$course_id.'"  class="btn" style="text-align: left; padding:0px;">
+                  <div class="card">
+                    <img src="'.str_replace('..', '.', $row['course_img']).'" class="card-img-top" alt="Guitar" />
+                    <div class="card-body">
+                      <h5 class="card-title">'.$row['course_name'].'</h5>
+                      <p class="card-text">'.$row['course_desc'].'</p>
+                    </div>
+                    <div class="card-footer">
+                      <p class="card-text d-inline">Price: <small><del>&#8377 '.$row['course_original_price'].'</del></small> <span class="font-weight-bolder">&#8377 '.$row['course_price'].'<span></p> <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+                    </div>
+                  </div>
+                </a>  ';
+            }
+          }
+        ?>  -->
+      </div>   
+      <div class="text-center m-2">
+        <a class="btn btn-danger btn-sm" href="courses.php">View All Course</a> 
+      </div>
+    </div>  <!-- End  Course -->
+
+     <?php 
+    include('view\courses.php'); 
+    ?> 
+
 
     <script src="view/js/jquery.min.js"></script>
     <script src="view/js/popper.min.js"></script>
