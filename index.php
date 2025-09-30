9518922f674563044ac853cea96c7f20207eb46c
Sulfur404 <mrnafis10@gmail.com>
2025-09-30 11:35:38 +0600

    Add element and fixed some bugs

--- index.php
+++ index.php
@@ -70,66 +70,61 @@
         </div>
     </div>
 
-    <div class="container mt-5"> <!-- Start Course -->
-      <h1 class="text-center">Popular Course</h1>
+      //Ai kkahne couse gula show korate hobe
       
-      <!-- <div class="card-deck mt-4"> //1st course cart
-        <?php
-        $sql = "SELECT * FROM course LIMIT 3";
-        $result = $conn->query($sql);
-        if($result->num_rows > 0){ 
-          while($row = $result->fetch_assoc()){
-            $course_id = $row['course_id'];
-            echo '
-            <a href="coursedetails.php?course_id='.$course_id.'" class="btn" style="text-align: left; padding:0px; margin:0px;">
-              <div class="card">
-                <img src="'.str_replace('..', '.', $row['course_img']).'" class="card-img-top" alt="Guitar" />
-                <div class="card-body">
-                  <h5 class="card-title">'.$row['course_name'].'</h5>
-                  <p class="card-text">'.$row['course_desc'].'</p>
-                </div>
-                <div class="card-footer">
-                  <p class="card-text d-inline">Price: <small><del>&#8377 '.$row['course_original_price'].'</del></small> <span class="font-weight-bolder">&#8377 '.$row['course_price'].'<span></p> <a class="btn btn-primary text-white font-weight-bolder float-right" href="coursedetails.php?course_id='.$course_id.'">Enroll</a>
-                </div>
-              </div>
-            </a>  ';
-          }
-        }
-        ?>   
-      </div>  
-      <div class="card-deck mt-4"> //2nd cart course
-        <?php
-          $sql = "SELECT * FROM course LIMIT 3,3";
-          $result = $conn->query($sql);
-          if($result->num_rows > 0){ 
-            while($row = $result->fetch_assoc()){
-              $course_id = $row['course_id'];
-              echo '
-                <a href="coursedetails.php?course_id='.$course_id.'"  class="btn" style="text-align: left; padding:0px;">
-                  <div class="card">
-                    <img src="'.str_replace('..', '.', $row['course_img']).'" class="card-img-top" alt="Guitar" />
-                    <div class="card-body">
-                      <h5 class="card-title">'.$row['course_name'].'</h5>
-                      <p class="card-text">'.$row['course_desc'].'</p>
-                    </div>
-                    <div class="card-footer">
-                      <p class="card-text d-inline">Price: <small><del>&#8377 '.$row['course_original_price'].'</del></small> <span class="font-weight-bolder">&#8377 '.$row['course_price'].'<span></p> <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
-                    </div>
-                  </div>
-                </a>  ';
-            }
-          }
-        ?>  -->
-      </div>   
-      <div class="text-center m-2">
-        <a class="btn btn-danger btn-sm" href="courses.php">View All Course</a> 
-      </div>
-    </div>  <!-- End  Course -->
-
      <?php 
-    include('view\courses.php'); 
+    include('C:\xampp\htdocs\Acadexa_A_Smart_Online_Learning_Management_System\view\contact.php');
     ?> 
 
+      //Student feedbad ar ar jonno code korte hobw and kisu external css and js apply korte hobe
+
+    <div class="container-fluid bg-danger">
+    <div class="row text-white text-center p-1">
+        <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-facebook-f"></i> Facebook</a>
+        </div>
+        <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-twitter"></i> Twitter</a>
+        </div>
+        <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-whatsapp"></i> WhatsApp</a>
+        </div>
+        <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-instagram"></i> Instagram</a>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid p-4" style="background-color:#E9ECEF">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-sm">
+                <h5>About Us</h5>
+                <p>Acadexa provides smart online learning solutions, offering access to quality courses for students across Bangladesh and beyond.</p>
+            </div>
+            <div class="col-sm">
+                <h5>Category</h5>
+                <a class="text-dark" href="#">Web Development</a><br>
+                <a class="text-dark" href="#">Web Designing</a><br>
+                <a class="text-dark" href="#">Android App Development</a><br>
+                <a class="text-dark" href="#">iOS Development</a><br>
+                <a class="text-dark" href="#">Data Analysis</a><br>
+            </div>
+            <div class="col-sm">
+                <h5>Contact Us</h5>
+                <p>
+                    Acadexa<br>
+                    Dhaka, Bangladesh<br>
+                    Phone: +880123456789<br>
+                    www.acadexa.com
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    //footer add kora lagebe
 
     <script src="view/js/jquery.min.js"></script>
     <script src="view/js/popper.min.js"></script>
--- view/contact.php
+++ view/contact.php
@@ -1,25 +1,22 @@
-<!--Start Contact Us-->
-<div class="container mt-4" id="Contact">  <!--Start Contact Us Container-->
-      <h2 class="text-center mb-4">Contact US</h2> <!-- Contact Us Heading -->
-      <div class="row">  <!--Start Contact Us Row-->
-        <div class="col-md-8"> <!--Start Contact Us 1st Column-->
-          <form action="" method="post">
-            <input type="text" class="form-control" name="name" placeholder="Name"><br>
-            <input type="text" class="form-control" name="subject" placeholder="Subject"><br>
-            <input type="email" class="form-control" name="email" placeholder="E-mail"><br>
-            <textarea class="form-control" name="message" placeholder="How can we help you?" style="height:150px;"></textarea><br>
-            <input class="btn btn-primary" type="submit" value="Send" name="submit"><br><br>
-          </form>
-        </div> <!-- End Contact Us 1st Column-->
-
-        <div class="col-md-4 stripe text-white text-center">  <!-- Start Contact Us 2nd Column-->
-          <h4>iSchool</h4>
-          <p>iSchool, 
-          Near Police Camp II, Bokaro, 
-          Jharkhand - 834005<br /> 
-          Phone: +00000000 <br />
-          www.ischool.com </p>
-        </div> <!-- End Contact Us 2nd Column-->
-      </div> <!-- End Contact Us Row-->
-    </div> <!-- End Contact Us Container-->
-    <!-- End Contact Us -->
+<div class="container mt-4" id="Contact">
+  <h2 class="text-center mb-4">Contact Us</h2>
+  <div class="row">
+    <div class="col-md-8">
+      <form action="" method="post">
+        <input type="text" class="form-control mb-3" name="name" placeholder="Name">
+        <input type="text" class="form-control mb-3" name="subject" placeholder="Subject">
+        <input type="email" class="form-control mb-3" name="email" placeholder="E-mail">
+        <textarea class="form-control mb-3" name="message" placeholder="How can we help you?" style="height:150px;"></textarea>
+        <input class="btn btn-primary" type="submit" value="Send" name="submit">
+      </form>
+    </div>
+    <div class="col-md-4 text-white stripe text-center ">
+      <br><h4>Acadexa</h4>
+      <p>
+        Dhaka, Bangladesh<br>
+        Phone: +8801734514433<br>
+        www.acadexa.com
+      </p>
+    </div>
+  </div>
+</div>
