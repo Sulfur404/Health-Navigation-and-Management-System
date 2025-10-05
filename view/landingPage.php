495d036617f92e9eea08a0ff6e9c93d3671f42a0
Sulfur404 <mrnafis10@gmail.com>
2025-10-05 09:47:56 +0600

    Add header Section and some css

--- view/landingPage.php
+++ view/landingPage.php
@@ -4,9 +4,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Health Navigation Management System</title>
-  
+  <link rel="stylesheet" href="../assets/css/landingpage.css">
+  <link rel="icon" href="../assets/image/HNMS.png?v=2" type="image/png" sizes="32x32">
 </head>
 <body>
+  <!-- Header -->
+  <?php include "../view/header.php";?>
   
 </body>
 </html>
