af66861be4e441de57e4d8be9a69eff1e770d8fd
Sulfur404 <mrnafis10@gmail.com>
2025-10-08 00:16:03 +0600

    add admin overview and add some picture

---
+++ view/adminOverview.php
@@ -0,0 +1,14 @@
+<div class="overview-container">
+    <div class="overview-card">
+        <h3>Number of Hospitals</h3>
+        <p>100</p>
+    </div>
+    <div class="overview-card">
+        <h3>Number of Doctors</h3>
+        <p>400</p>
+    </div>
+    <div class="overview-card">
+        <h3>Number of Patients</h3>
+        <p>10000</p>
+    </div>
+</div>
