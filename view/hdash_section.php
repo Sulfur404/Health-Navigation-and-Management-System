f5e6a01bd47d0a7192eb59f454fef1d5ccdc8bf5
Asraful Islam <islam.mdasraful1854@gmail.com>
2025-10-10 00:23:53 +0600

    added hospital dashboard

---
+++ view/hdash_section.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <div id="dashboard" class="section" style="display:block;">
+        <div class="welcome-banner">
+          <h2>Quick overview of your health activities</h2>
+        </div>
+        <div class="dashboard-cards">
+          <div class="card">
+            <h3>Today's Appointments</h3>
+            <ul id="appointmentsList">
+              <li>No appointments scheduled.</li>
+            </ul>
+          </div>
+          <div class="card">
+            <h3>Pending/Cancelled Appointmnets</h3>
+          <p><span id="pendingapps">0</span> Appointments</p>
+          </div>
+          
+          <div class="card">
+      <h3>Bed Availability</h3>
+      <p><span id="bedsAvailable">25</span> Beds</p>
+    </div> 
+    <div class="card">
+      <h3>More Services</h3>
+      
+    </div> 
+         <div class="card">
+  <h3>More Upcoming Features</h3>
+  <ul>
+    <li>Patient Teleconsultation</li>
+    <li>Automated Prescription Generation</li>
+    <li>Inventory Management</li>
+    <li>Staff Scheduling</li>
+    <li>Health Analytics & Reports</li>
+  </ul>
+</div>
+
+        </div>
+      </div>
+</body>
+</html>
