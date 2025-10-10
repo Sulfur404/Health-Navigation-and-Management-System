861260fe1c5ec4a292cd103f5d348d5859124ab2
Sulfur404 <mrnafis10@gmail.com>
2025-10-10 10:48:44 +0600

    add my appointment files

---
+++ view/my_appointments.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="section" id="appointmentsSection">
+    <h3>My Appointments</h3>
+    <div class="appointments-table-container">
+        <table class="appointments-table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Doctor</th>
+                    <th>Department</th>
+                    <th>Hospital</th>
+                    <th>Address</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody id="appointmentsBody">
+            </tbody>
+        </table>
+    </div>
+</div>
+
+</body>
+</html>
