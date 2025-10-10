09a23f1aaa624db7d41f195222518a0494f4d686
Sulfur404 <mrnafis10@gmail.com>
2025-10-10 21:52:45 +0600

    Add patirnt history file4

---
+++ view/patient_history.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="section" id="historySection">
+    <h3>My History</h3>
+
+    <div class="history-table-container">
+        <table class="history-table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Doctor</th>
+                    <th>Department</th>
+                    <th>Hospital</th>
+                    <th>Address</th>
+                    <th>Date</th>
+                    <th>Diagnosis</th>
+                    <th>Treatment</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody id="historyBody">
+                <!-- Rows injected via JS -->
+            </tbody>
+        </table>
+    </div>
+</div>
+
+</body>
+</html>
