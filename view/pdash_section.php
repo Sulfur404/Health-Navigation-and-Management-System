439d2f346bc8c45f4876ad720d145e76436297d3
Sulfur404 <mrnafis10@gmail.com>
2025-10-10 17:06:21 +0600

    add patient dashboard section

---
+++ view/pdash_section.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+      <div id="dashboard" class="section" style="display:block;">
+        <div class="welcome-banner">
+          <h2>Quick overview of your health activities</h2>
+        </div>
+        <div class="dashboard-cards">
+          <div class="card">
+            <h3>Upcoming Appointments</h3>
+            <ul id="appointmentsList">
+              <li>No appointments scheduled.</li>
+            </ul>
+          </div>
+          <div class="card">
+            <h3>Nearest Hospital / Pharmacy</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d29199.16045070734!2d90.42967790596899!3d23.82233052693511!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1shospital%2C%20pharrma%20near%20me!5e0!3m2!1sen!2sbd!4v1758910592189!5m2!1sen!2sbd" frameborder="0"
+            border-radius="10px "width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+          </div>
+          <div class="card">
+            <h3>Health Tips</h3>
+            <ul>
+              <li>Stay hydrated by drinking at least 8 glasses of water daily.</li>
+              <li>Incorporate at least 30 minutes of exercise into your daily routine.</li>
+              <li>Eat a balanced diet rich in fruits, vegetables, and whole grains.</li>
+              <li>Ensure you get 7-8 hours of quality sleep each night.</li>
+              <li>Practice stress-relief techniques such as meditation or deep breathing.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+</body>
+</html>
